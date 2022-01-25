6de5bd891451f6c204c14cde9a3eada26d86f157
tanguyl <tanguy.losseau@student.uclouvain.be>
2022-01-25 17:33:10 +0100

    Print a sphere.

--- src/raytracer.erl
+++ src/raytracer.erl
@@ -19,12 +19,26 @@ unit_vec(Vec={matrix,_,_,_})->
 ray_at(#ray{orig=O, dir=D}, t) ->
     numerl:plus(O, numerl:mult(D,t)).
 
-ray_color(#ray{dir=D})->
-    T = (numerl:at(unit_vec(D),2) + 1) / 2.0,
-    %T = (1+numerl:at(D,2))/2,
-    R = numerl:mult(numerl:add(numerl:mult(vec3(1,1,1),1-T), numerl:mult(vec3(0.5, 0.7, 1.0),T)),255),
-    R.
+ray_color(Ray=#ray{dir=D})->
+    HitShpere = hit_sphere(vec3(0,0,-1), 0.5, Ray),
+    if HitShpere ->
+        vec3(255,0,0);
+    true->
+        T = (numerl:at(unit_vec(D),2) + 1) / 2.0,
+        %T = (1+numerl:at(D,2))/2,
+        R = numerl:mult(numerl:add(numerl:mult(vec3(1,1,1),1-T), numerl:mult(vec3(0.5, 0.7, 1.0),T)),255),
+        R
+    end.
 
+%Sphere-related functions.
+%----------------------------------------------------
+hit_sphere(Center, Radius, #ray{orig=O, dir=D})->
+    Oc = numerl:sub(O,Center),
+    A = numerl:ddot(D,D),
+    B = numerl:ddot(Oc,D)*2,
+    C = numerl:ddot(Oc,Oc) - Radius * Radius,
+    Discri = B*B - 4*A*C,
+    Discri > 0. 
 
 %Render-related functions.
 %----------------------------------------------------
@@ -46,26 +60,12 @@ p3()->
     Origin = vec3(0, 0, 0),
     Horizontal = vec3(Viewport_width, 0, 0),
     Vertical = vec3(0, Viewport_height, 0),
-    Lower_left_corner = numerl:sub(
-                            numerl:sub(
-                                numerl:sub(
-                                    Origin,
-                                    numerl:divide(Horizontal,2)),
-                                numerl:divide(Vertical,2)),
-                            vec3(0, 0, Focal_length)),
-    %io:format("Lower left corner is at ~p ~n", [numerl:print(Lower_left_corner)]),
+    Lower_left_corner = numerl:eval([Origin, sub, numerl:divide(Horizontal,2), sub, numerl:divide(Vertical,2), sub, vec3(0,0, Focal_length)]),
 
     GenPixel = fun(I,J)->
                     U = I/(Image_width-1),
                     V = J/(Image_height-1),
-                    %lower_left_corner + u*horizontal + v*vertical - origin
-                    R = #ray{orig=Origin, dir=numerl:sub(
-                                                numerl:add(
-                                                    numerl:add(
-                                                        Lower_left_corner,
-                                                        numerl:mult(Horizontal,U)),
-                                                    numerl:mult(Vertical, V)),
-                                                Origin)},
+                    R = #ray{orig=Origin, dir=numerl:eval([Lower_left_corner, add, numerl:mult(Horizontal,U), add, numerl:mult(Vertical,V), sub, Origin])},
                     Color = ray_color(R),
 
                     if I == 0   -> [J, Color];
