3cf24dc0234af5c0a105dee02577b8fa663fa493
tanguyl <tanguy.losseau@student.uclouvain.be>
2022-01-25 15:02:04 +0100

    Render a background.

--- src/raytracer.erl
+++ src/raytracer.erl
@@ -2,30 +2,76 @@
 
 -export([p3/0]).
 
+%Vector related function.
+%----------------------------------------------------
+
+vec3(C0,C1,C2)->
+    numerl:matrix([[C0,C1,C2]]).
+
+unit_vec(Vec={matrix,_,_,_})->
+    Nrm = numerl:dnrm2(Vec),
+    numerl:divide(Vec, Nrm*Nrm).
 
 %Ray-related functions.
--record(ray, {orig, dest}).
+%----------------------------------------------------
+-record(ray, {orig, dir}).
 
-ray_at(#ray{orig=O, dest=D}, t) ->
+ray_at(#ray{orig=O, dir=D}, t) ->
     numerl:plus(O, numerl:mult(D,t)).
+
+ray_color(#ray{dir=D})->
+    T = (numerl:at(unit_vec(D),2) + 1) / 2.0,
+    %T = (1+numerl:at(D,2))/2,
+    R = numerl:mult(numerl:add(numerl:mult(vec3(1,1,1),1-T), numerl:mult(vec3(0.5, 0.7, 1.0),T)),255),
+    R.
 
 
 %Render-related functions.
-
+%----------------------------------------------------
 write_color(S, Vector)->
     io:format(S, "~B ~B ~B~n", numerl:mtfli(Vector)).
 
 p3()->
-    Image_width = 255,
-    Image_height = 255,
+    %Screen.
+    Aspect_ratio = 16/9,
+    Image_width = 400,
+    Image_height = round(Image_width / Aspect_ratio),
 
-    Factors = numerl:matrix([[255.99/(Image_width-1), 255.99/(Image_height-1), 0.25*255.99]]),
+
+    %Camera.
+    Viewport_height = 2.0,
+    Viewport_width = Aspect_ratio * Viewport_height,
+    Focal_length = 1.0,
+
+    Origin = vec3(0, 0, 0),
+    Horizontal = vec3(Viewport_width, 0, 0),
+    Vertical = vec3(0, Viewport_height, 0),
+    Lower_left_corner = numerl:sub(
+                            numerl:sub(
+                                numerl:sub(
+                                    Origin,
+                                    numerl:divide(Horizontal,2)),
+                                numerl:divide(Vertical,2)),
+                            vec3(0, 0, Focal_length)),
+    %io:format("Lower left corner is at ~p ~n", [numerl:print(Lower_left_corner)]),
 
     GenPixel = fun(I,J)->
-                    Color = numerl:mult(numerl:matrix([[I,J,1]]), Factors),
+                    U = I/(Image_width-1),
+                    V = J/(Image_height-1),
+                    %lower_left_corner + u*horizontal + v*vertical - origin
+                    R = #ray{orig=Origin, dir=numerl:sub(
+                                                numerl:add(
+                                                    numerl:add(
+                                                        Lower_left_corner,
+                                                        numerl:mult(Horizontal,U)),
+                                                    numerl:mult(Vertical, V)),
+                                                Origin)},
+                    Color = ray_color(R),
+
                     if I == 0   -> [J, Color];
                     true        -> Color end
                 end,
+
     Picture = [[ GenPixel(I,J) || I <- lists:seq(0,Image_width-1)] || J <- lists:seq(Image_height-1,0, -1)],
     
     {ok, S} = file:open("image.ppm", [write, {delayed_write, 700000, 40}]),
@@ -43,6 +89,3 @@ p3()->
     io:format(S, "P3\n~B ~B\n255\n", [Image_width, Image_height]),
     WriteLines(lists:flatten(Picture)),
     file:close(S).
-    
-
-    
