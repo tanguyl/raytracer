b4635a76061c51855cdd2c67356df4932a5064fa
tanguyl <tanguy.losseau@student.uclouvain.be>
2022-01-25 11:45:41 +0100

    Increasing p3 efficiency.

--- src/raytracer.erl
+++ src/raytracer.erl
@@ -1,31 +1,37 @@
 -module(raytracer).
 
--export([p3_numerl/0]).
+-export([p3/0]).
 
 
 write_color(S, Vector)->
-    C = numerl:dot(Vector, 255),
-    io:format(S, "~B ~B ~B~n", [round(numerl:at(C,1)), round(numerl:at(C,2)), round(numerl:at(C,3))]).
+    io:format(S, "~B ~B ~B~n", numerl:mtfli(Vector)).
 
-p3_numerl()->
+p3()->
     Image_width = 255,
     Image_height = 255,
 
-    GenPixel = fun(I,J)->[J, numerl:matrix([[I/(Image_width-1), J/(Image_height-1), 0.25]])] end,
-    Picture = [[ GenPixel(I,J) || I <- lists:seq(0,Image_width)] || J <- lists:seq(Image_height-1,-1, -1)],
+    Factors = numerl:matrix([[255.99/(Image_width-1), 255.99/(Image_height-1), 0.25*255.99]]),
 
-
+    GenPixel = fun(I,J)->
+                    Color = numerl:mult(numerl:matrix([[I,J,1]]), Factors),
+                    if I == 0   -> [J, Color];
+                    true        -> Color end
+                end,
+    Picture = [[ GenPixel(I,J) || I <- lists:seq(0,Image_width-1)] || J <- lists:seq(Image_height-1,0, -1)],
     
-    {ok, S} = file:open("image.ppm", [write]),
+    {ok, S} = file:open("image.ppm", [write, {delayed_write, 700000, 40}]),
     WriteLines = 
         fun F([])->ok;
-         F([J, Colors|T])->
-             io:format("\r~B lines remaining", [J]),
-             write_color(S, Colors),
+         F([Colors|T])->
+                if is_tuple(Colors)->
+                    write_color(S, Colors);
+                true ->
+                    io:format("\r~B remaining.", [Colors])
+                end,
              F(T)
         end,
 
-    io:format(S, "P3\n~B ~B\n255\n", [Image_width+1, Image_height+1]),
+    io:format(S, "P3\n~B ~B\n255\n", [Image_width, Image_height]),
     WriteLines(lists:flatten(Picture)),
     file:close(S).
     
